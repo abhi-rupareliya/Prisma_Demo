be0d4f5807b5bbf78de5bd222cdc68d1be3d1cab
abhi-rupareliya <abhirupareliya25@gmail.com>
2023-08-02 20:02:33 +0530

    init

---
+++ Readme.md
@@ -0,0 +1,8 @@
+### add the Prisma CLI as a development dependency 
+#### npm install prisma --save-dev
+### invoke the Prisma CLI
+#### npx prisma
+### set up your Prisma project by creating your Prisma schema file with the following command:
+#### npx prisma init
+### To map your data model to the database schema, you need to use the prisma migrate CLI commands:
+#### npx prisma migrate dev --name init
---
+++ index.js
@@ -0,0 +1,50 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+// async function main() {
+//   // ... you will write your Prisma Client queries here
+//   const allUsers = await prisma.user.findMany();
+//   console.log(allUsers);
+// }
+
+// async function main() {
+//   await prisma.user.create({
+//     data: {
+//       name: "Alice",
+//       email: "alice@prisma.io",
+//       posts: {
+//         create: { title: "Hello World" },
+//       },
+//       profile: {
+//         create: { bio: "I like turtles" },
+//       },
+//     },
+//   });
+
+//   const allUsers = await prisma.user.findMany({
+//     include: {
+//       posts: true,
+//       profile: true,
+//     },
+//   });
+//   console.dir(allUsers, { depth: null });
+// }
+
+async function main() {
+  const post = await prisma.post.update({
+    where: { id: 1 },
+    data: { published: true, content: "hello world!!!!" },
+  });
+  console.log(post);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
